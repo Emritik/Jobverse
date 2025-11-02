9fede08346384d3369432e9ef6419c6d5b132a18
Emritik <sharmar152004@gmail.com>
2025-11-02 21:02:27 +0530

    Fix final changes in code  by adding new routes.

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 import { singleUpload } from "../middleware/multer.js";
+import { getCurrentUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router.route("/register").post(singleUpload, register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated, singleUpload, updateProfile);
+router.get("/me", isAuthenticated, getCurrentUser);
 
 export default router;
